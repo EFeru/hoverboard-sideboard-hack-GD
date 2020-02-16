7a8071df61a901e202f86d08dbf6cfb9692bc74f
EmanuelFeru <aerdronix@gmail.com>
2020-02-16 22:08:20 +0100

    updated Keil project

--- Inc/config.h
+++ Inc/config.h
@@ -22,9 +22,13 @@
 #define CONFIG_H
 
 /* ==================================== VARIANT SELECTION ==================================== */
+// PlatformIO: uncomment desired variant in platformio.ini
+// Keil uVision: select desired variant from the Target drop down menu (to the right of the Load button)
+// Ubuntu: define the desired build variant here if you want to use make in console
+// or use VARIANT environment variable for example like "make -e VARIANT=VARIANT_DEBUG". Select only one at a time.
 #if !defined(PLATFORMIO)
-  #define VARIANT_DEBUG     									// Variant for debugging and checking the capabilities of the side-board
-  //#define VARIANT_HOVERBOARD  								// Variant for using the side-boards connected to the Hoverboard mainboard
+  // #define VARIANT_DEBUG     								// Variant for debugging and checking the capabilities of the side-board
+  // #define VARIANT_HOVERBOARD  							// Variant for using the side-boards connected to the Hoverboard mainboard
 #endif
 
 /* ==================================== DO NOT TOUCH SETTINGS ==================================== */
